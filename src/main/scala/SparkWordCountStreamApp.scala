69d41c8836341274942e378276c0c6d3341c3648
FunsoIyaju <stepheniyaju@gmail.com>
2022-06-18 21:00:28 +0000

    Initial commit

---
+++ src/main/scala/SparkWordCountStreamApp.scala
@@ -0,0 +1,27 @@
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.storage.StorageLevel
+
+object SparkWordCountStreamApp
+{
+  def main(args: Array[String]): Unit={
+    val spark = SparkSession.builder
+      .master("local[*]")
+      .appName(getClass.getSimpleName)
+      .getOrCreate()
+
+    val context = spark.sparkContext
+
+    val streamContext = new StreamingContext(context, Seconds(40))
+    streamContext.checkpoint("D:\\Checkpoint")
+
+    val lines = streamContext.socketTextStream("localhost", 9999, StorageLevel.MEMORY_AND_DISK_SER)
+    val words = lines.flatMap(_.split(" "))
+    val pairs = words.map(word => (word, 1))
+    val wordcount = pairs.reduceByKey(_ + _)
+    wordcount.print()
+    streamContext.start()
+    streamContext.awaitTermination()
+  }
+
+}
